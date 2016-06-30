368e18d47fb0b2053127322baa8736fe2b8b0654
scj1656 <scj1656@163.com>
2016-06-30 13:44:32 +0800

    创建索引与查询

--- src/main/java/com/solar/lucene/LuceneReader.java
+++ src/main/java/com/solar/lucene/LuceneReader.java
@@ -1,5 +1,40 @@
 package com.solar.lucene;
 
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.Version;
+
 public class LuceneReader {
 
+    private Directory directory;
+
+    private Analyzer  analyzer;
+
+    public LuceneReader(Directory directory, Analyzer analyzer) {
+        this.directory = directory;
+        this.analyzer = analyzer;
+    }
+
+    public void read() throws IOException, ParseException {
+        DirectoryReader reader = DirectoryReader.open(directory);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, "fileName", analyzer);
+        Query query = parser.parse("text");
+        ScoreDoc[] hits = searcher.search(query, null, 1000).scoreDocs;
+        for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = searcher.doc(hits[i].doc);
+            System.out.println(hitDoc.get("fieldname"));
+        }
+        reader.close();
+        directory.close();
+    }
 }
--- src/main/java/com/solar/lucene/LuceneWriter.java
+++ src/main/java/com/solar/lucene/LuceneWriter.java
@@ -1,5 +1,37 @@
 package com.solar.lucene;
 
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.Version;
+
 public class LuceneWriter {
 
+    private Directory directory;
+    private Analyzer  analyzer;
+
+    public LuceneWriter() {
+
+    }
+
+    public LuceneWriter(Directory directory, Analyzer analyzer) {
+        this.directory = directory;
+        this.analyzer = analyzer;
+    }
+
+    public void createIndex() throws IOException {
+        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_CURRENT, analyzer);
+        IndexWriter writer = new IndexWriter(directory, config);
+        Document doc = new Document();
+        String text = "this is lucene test";
+        doc.add(new Field("fileName", text, TextField.TYPE_STORED));
+        writer.addDocument(doc);
+        writer.close();
+    }
 }
